3de22d9d71b24fd72bd9f6b6e23b34c0e7b8abb6
Shine-B <abc@eqxample.com>
2020-11-07 03:51:36 +0900

    四則演算

---
+++ Lesson_2/Sample_02.java
@@ -0,0 +1,19 @@
+class Sample_02{
+    static int value1 = 150;
+    static int value2 = 820;
+    static int value3 = 1400;
+    static int value4 = 1980;
+    public static void main(String[] args){
+        // value1 ‚Æ value2 ‚ð‘«‚µ‚Ä‚ ‚°‚Ü‚µ‚å‚¤
+        System.out.println((value1 + value2));
+        // value4 ‚©‚ç value2 ‚ðˆø‚¢‚Ä‚ ‚°‚Ü‚µ‚å‚¤
+        System.out.println(value4 - value2);
+        // value3 ‚É 3 ‚ð‚©‚¯‚Ä‚ ‚°‚Ü‚µ‚å‚¤
+        System.out.println(value3 * 3);
+        // value4 ‚ð 6 ‚ÅŠ„‚Á‚Ä‚ ‚°‚Ü‚µ‚å‚¤
+        System.out.println(value4 / 6);
+        // value3 ‚ð 3 ‚ÅŠ„‚Á‚Ä‚ ‚°‚Ü‚µ‚å‚¤
+        System.out.println(value3 / 3);
+    }
+}
+
