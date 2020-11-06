03e7da6fb67ed6494ccf86400b910f87228e25f2
Shine-B <abc@eqxample.com>
2020-11-07 03:55:05 +0900

    自作メソッド

---
+++ Lesson_4/Sample_04.java
@@ -0,0 +1,11 @@
+class Sample_04{
+
+    public static void main(String[] args){
+        自作メソッド();
+    }
+
+    public static void 自作メソッド(){
+        System.out.println("自作メソッドを作成し、");
+        System.out.println("そこでメッセージを呼び出しているよ！");
+    }
+}
---
+++ Lesson_4/Sample_04_01.java
@@ -0,0 +1,11 @@
+class Sample_04_01{
+    public static void main(String[] args){
+        自作メソッド();
+    }
+
+    public static void 自作メソッド(){
+        System.out.println("5 + 1の結果は");
+        System.out.println(5+1);
+        System.out.println("です。");
+    }
+}
